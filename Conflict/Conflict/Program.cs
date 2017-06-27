e5ed2e6e0d87713c50a2af059762ecea0c66dff9
Andrei Profir <andrei.profir@outlook.com>
2017-06-27 12:59:03 +0300

    Some

---
+++ Conflict/Conflict/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conflict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I love C#,");
+            Console.WriteLine("and");
+            Console.WriteLine("C# love me too! :D");
+        }
+    }
+}
